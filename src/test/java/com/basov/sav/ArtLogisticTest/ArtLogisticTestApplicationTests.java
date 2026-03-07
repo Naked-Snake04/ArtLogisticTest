6f09b170f3522c838579e5794fabad1171a93ae5
Naked_Snake04 <77673071+Naked-Snake04@users.noreply.github.com>
2026-03-07 14:01:37 +0300

    Добавление теста на логин

--- src/test/java/com/basov/sav/ArtLogisticTest/ArtLogisticTestApplicationTests.java
+++ src/test/java/com/basov/sav/ArtLogisticTest/ArtLogisticTestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.basov.sav.ArtLogisticTest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ArtLogisticTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	MockMvc mockMvc;
 
+	@Test
+	void testLogin() throws Exception {
+		mockMvc.perform(post("/account/login")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{\"username\":\"sa_apitest\",\"password\":\"Zuvo4715\"}"))
+				.andExpect(status().isOk());
+	}
 }
