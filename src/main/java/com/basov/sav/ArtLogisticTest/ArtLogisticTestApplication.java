b9c27eb1c8d6e1528f386abb701389d6505aad33
Naked-Snake04 <joKer041200saV@gmail.com>
2026-03-07 09:09:44 +0300

    Init Commit

---
+++ src/main/java/com/basov/sav/ArtLogisticTest/ArtLogisticTestApplication.java
@@ -0,0 +1,13 @@
+package com.basov.sav.ArtLogisticTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtLogisticTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtLogisticTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basov/sav/ArtLogisticTest/ArtLogisticTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basov.sav.ArtLogisticTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtLogisticTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
