31ef5552c4ec6fa94b19f455c04dda9a0fa49515
Naked-Snake04 <joKer041200saV@gmail.com>
2026-03-09 15:00:26 +0300

    Исправление контроллера и теста для логина

--- src/main/java/com/basov/sav/ArtLogisticTest/Controllers/AccountLoginController.java
+++ src/main/java/com/basov/sav/ArtLogisticTest/Controllers/AccountLoginController.java
@@ -2,14 +2,27 @@ package com.basov.sav.ArtLogisticTest.Controllers;
 
 import com.myapp.api.AccountApi;
 import com.myapp.model.AccountCredentials;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountLoginController implements AccountApi {
+    static String USERNAME = "sa_apitest";
+    static String PASSWORD = "Zuvo4715";
 
     @Override
     public ResponseEntity<String> login(AccountCredentials accountCredentials) {
-        return ResponseEntity.ok("{\"token\": \"fake-token\"");
+        if (checkAccountCredentials(accountCredentials)) {
+            return ResponseEntity.ok("{\"access\": \"true\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Forbidden");
+        }
+    }
+
+    private static boolean checkAccountCredentials(AccountCredentials accountCredentials) {
+        return accountCredentials != null &&
+                accountCredentials.getUsername().equals(USERNAME) &&
+                accountCredentials.getPassword().equals(PASSWORD);
     }
 }
