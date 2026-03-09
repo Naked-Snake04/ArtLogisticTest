77e2a3a57355c068c06a5b20efa4d0a533fa9389
Naked-Snake04 <joKer041200saV@gmail.com>
2026-03-09 15:11:48 +0300

    Изменение присваивания логина и пароля

--- src/main/java/com/basov/sav/ArtLogisticTest/Controllers/AccountLoginController.java
+++ src/main/java/com/basov/sav/ArtLogisticTest/Controllers/AccountLoginController.java
@@ -2,14 +2,18 @@ package com.basov.sav.ArtLogisticTest.Controllers;
 
 import com.myapp.api.AccountApi;
 import com.myapp.model.AccountCredentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountLoginController implements AccountApi {
-    static String USERNAME = "sa_apitest";
-    static String PASSWORD = "Zuvo4715";
+    @Value("${test.login.username}")
+    private String USERNAME;
+
+    @Value("${test.login.password}")
+    private String PASSWORD;
 
     @Override
     public ResponseEntity<String> login(AccountCredentials accountCredentials) {
@@ -20,7 +24,7 @@ public class AccountLoginController implements AccountApi {
         }
     }
 
-    private static boolean checkAccountCredentials(AccountCredentials accountCredentials) {
+    private boolean checkAccountCredentials(AccountCredentials accountCredentials) {
         return accountCredentials != null &&
                 accountCredentials.getUsername().equals(USERNAME) &&
                 accountCredentials.getPassword().equals(PASSWORD);
