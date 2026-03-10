306409d8009de8d535ba5fe03e317eeb1b6f9a20
Naked_Snake04 <77673071+Naked-Snake04@users.noreply.github.com>
2026-03-10 11:14:36 +0300

    Исправление контроллера и теста

--- src/main/java/com/basov/sav/ArtLogisticTest/Controllers/RequestController.java
+++ src/main/java/com/basov/sav/ArtLogisticTest/Controllers/RequestController.java
@@ -3,6 +3,7 @@ package com.basov.sav.ArtLogisticTest.Controllers;
 import com.myapp.api.RequestApi;
 import com.myapp.model.SalesChannel;
 import com.myapp.model.Services;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,10 @@ public class RequestController implements RequestApi {
                                               String invoiceNr,
                                               String orderNr,
                                               SalesChannel salesChannel) {
-        return ResponseEntity.ok("{\"ticketId\": \"T-12345\"}");
+        boolean checkService = service == Services.STOCKSTATE;
+        boolean checkDate = dateTo != null;
+
+        if (checkService && checkDate) return ResponseEntity.ok("stock-item");
+        else return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Forbidden");
     }
 }
